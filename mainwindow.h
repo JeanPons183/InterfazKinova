27c1dd0e00b53cd06087ff49c253c325271ec2d5
JeanPons <jeanpons@hotmail.es>
2023-08-19 12:19:01 -0600

    Practicando con los radialbutton

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "qspinbox.h"
 #include <QMainWindow>
+#include <QLCDNumber>
+#include <QDoubleSpinBox>
+
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_PDSel_clicked();
+
+    void on_PDSel_toggled(bool checked);
+
+    void on_KP1_valueChanged(double arg1);
+
+    void on_TiempoS_valueChanged(int arg1);
+
+    void on_KD1_valueChanged(double arg1);
+
 private:
     Ui::MainWindow *ui;
+
 };
 #endif // MAINWINDOW_H
