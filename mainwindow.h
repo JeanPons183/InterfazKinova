402912a2a4a15e9849f40359c9fe14bd8e3d53fc
Jean Pons <jeanpons@hotmail.es>
2023-09-12 10:14:01 -0600

    Volviendo

--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -55,61 +54,3 @@ private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private slots:
-
-    void Labels(QString Selected);
-
-    void on_ConectarPB_clicked();
-
-    void on_PlayPausePB_clicked();
-
-    void on_StopPB_clicked();
-
-    void on_Graficas_checkBox_stateChanged(int arg1);
-
-    void on_GuardarTrayectoriaPB_clicked();
-
-    void on_cambiarGainsPB_clicked();
-
-    void on_CambiarControlPB_clicked();
-
-    void on_CambiarTiempoPB_clicked();
-
-    void on_CambiarQdPB_clicked();
-
-    void on_PosPackPB_clicked();
-
-    void on_PosZeroPB_clicked();
-
-    void on_radioButton_clicked(bool checked);
-
-    void on_AgregarGraficaPB_clicked();
-
-    void on_EliminarGraficaPB_clicked();
-
-    void Robot();
-
-
-private:
-    Ui::MainWindow *ui;
-};
-#endif // MAINWINDOW_H
->>>>>>> Jean
