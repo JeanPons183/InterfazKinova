db48d508bd417677d838e1e862011efa846ed449
Jean Bryan Pons Cisneros <jeanpons@hotmail.es>
2023-10-11 21:13:39 -0600

    Modificando el main pa'probar lo que dice el Cesar

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 
 #include <QApplication>
 
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    std::cout << "Hola mundo" << std::endl;
     return a.exec();
 }
