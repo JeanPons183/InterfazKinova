aca5488a9325fdf08c8f98c4d754ea3989fc2934
Rodolfo Arzat <134309632+RodolfoArzat@users.noreply.github.com>
2023-11-09 08:56:40 -0600

    created .cpp to connect to robot

--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 
 #include <QApplication>
 
-#include <stdio.h>
-#include <iostream>
-
-using namespace std;
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    std::cout << "Hola mundo" << std::endl;
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QTimer>
-#include "qcustomplot.h"
-#include "ui_mainwindow.h"
-#include <QMessageBox>
-#include <QDesktopServices>
-#include <iostream>
-#include <qvector.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,93 +12,15 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    //Configuración Inicial de la Ventana Principal------------------------------------------------------------------------------------------------------
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
 
-private slots:
+    int RobotConnect(int argc, char **argv);
 
-    // Configuración de la ventana Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void PausarUI();
+    bool move_to_zeros_position(k_api::Base::BaseClient* base);
 
-    void PlayUI();
-
-    void Controlador(int select);
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    // Conectar Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_ConectarPB_clicked();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    // Robot Settings Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_CambiarControlPB_clicked();
-
-    void on_CambiarTiempoPB_clicked();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    // Ganancias Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_cambiarGainsPB_clicked();
-
-    void SaveValues();
-
-    void SetValues();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    // Posicion Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_PosZeroPB_clicked();
-
-    void on_PosPackPB_clicked();
-
-    void on_CambiarQdPB_clicked();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    // Ejecucion Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_IniciarPB_clicked();
-
-    void on_StopPB_clicked();
-
-    void on_CancelarGravedadPB_clicked();
-
-    void on_RegresarZeroPB_clicked();
-
-    void on_FinalizarPB_clicked();
-
-    void on_FinalizarPB_2_clicked();
-
-    void on_AccionTerminadaRB_toggled(bool checked);
-
-    void Temporizador();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    //Graficas Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_ActivarGraficasPB_clicked();
-
-    void CrearGrafica();
-
-    void ConfigurarGrafica(int index);
-
-    void GraficarTrayectoria(int index);
-
-    void ActualizarGrafica();
-
-    void on_ElegirGraficaCB_currentIndexChanged(int index);
-
-    void on_GuardarTrayectoriaPB_clicked();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    // MenuBar Frame
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
-    void on_actionKinova_Gen_3_Lite_User_Manual_triggered();
-
-    void on_actionControl_IV_Class_Notes_triggered();
-    //---------------------------------------------------------------------------------------------------------------------------------------------------
 
 private:
     Ui::MainWindow *ui;
-    double tiempo;
-    QTimer * miTemporizador;    // Variables y objeto de la clase QTimer, al conectarse con una funcion puede activarla al pasar una establecida cantidad de milisegundos
-    QCustomPlot *grafica = ui->WidgetGrafica;
 };
 #endif // MAINWINDOW_H
